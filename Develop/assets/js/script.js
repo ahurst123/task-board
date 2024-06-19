870c3e61eb0f252911d33a9bb9e69ce9bd1da2d4
ahurst123 <austinhurst123@gmail.com>
2024-06-18 20:36:29 -0700

    created the function handle drop to handle the dragging and dropping of the tasks on the task board.

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -19,7 +19,7 @@ function createTaskCard(task) {
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+    let 
 }
 
 // Todo: create a function to handle adding a new task
@@ -34,7 +34,12 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+    let taskID = ui.draggable.attr("id").split("-")[1];
+    let newStatus = $(this).attr("id");
+    let taskIndex = taskList.findIndex(task => task.id == taskID);
+    taskList[taskIndex].status = newStatus;
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    renderTaskList();
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
